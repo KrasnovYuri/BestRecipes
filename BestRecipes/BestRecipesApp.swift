7d1a394e56079c6058a1f70028d9f6799e5b3174
KrasnovYuri <krasnov1yuri@gmail.com>
2024-06-30 16:37:05 +0300

    start

---
+++ BestRecipes/BestRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestRecipesApp.swift
+//  BestRecipes
+//
+//  Created by Юрий on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BestRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestRecipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BestRecipes
+//
+//  Created by Юрий on 30.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
