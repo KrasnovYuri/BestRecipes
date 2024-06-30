d37563c573724e6c530bb81d31a1add7791f4621
KrasnovYuri <krasnov1yuri@gmail.com>
2024-06-30 22:12:28 +0300

    add images, extensions, fonts, icons

---
+++ BestRecipes/Extensions/Color+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Ext.swift
+//  BestRecipes
+//
+//  Created by Юрий on 30.06.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let сustomWhite = Color("White")
+    static let customRed = Color("Red")
+    static let customBlack = Color("Black")
+    static let customGray = Color("Gray")
+    static let customNeutral = Color("Neutral")
+    static let customGold = Color("Gold")
+}
+
+
---
+++ BestRecipes/Extensions/Fonts.swift
@@ -0,0 +1,18 @@
+//
+//  Fonts.swift
+//  BestRecipes
+//
+//  Created by Юрий on 30.06.2024.
+//
+
+import Foundation
+
+enum Poppins {
+    static let black = "Poppins-Black"
+    static let bold = "Poppins-Bold"
+    static let extraBold = "Poppins-ExtraBold"
+    static let light = "Poppins-Light"
+    static let medium = "Poppins-Medium"
+    static let regular = "Poppins-Regular"
+    static let semiBold = "Poppins-SemiBold"
+}
