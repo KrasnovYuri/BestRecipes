a45c2c870cfcd287393db555a5e153b6d2de8b2c
KrasnovYuri <krasnov1yuri@gmail.com>
2024-07-01 16:56:33 +0300

    add testView

---
+++ BestRecipes/Services/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  BestRecipes
+//
+//  Created by Юрий on 01.07.2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
