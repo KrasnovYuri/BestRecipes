30ec2b365431850415f09824953241d9ff9b7fad
KrasnovYuri <krasnov1yuri@gmail.com>
2024-07-08 15:02:37 +0300

    fix

---
+++ BestRecipes/Screens/Elements/BackButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  BackButtonView.swift
+//  BestRecipes
+//
+//  Created by Юрий on 08.07.2024.
+//
+
+import SwiftUI
+
+struct BackButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButtonView()
+}
