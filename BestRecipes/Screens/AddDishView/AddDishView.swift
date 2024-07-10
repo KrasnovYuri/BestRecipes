120dc34cdffb81faa182aa067a393f6d18be1a2a
ARuslanDenisov <angess47@gmail.com>
2024-07-10 19:12:02 +0700

    Add some fixex

--- BestRecipes/Screens/AddDishView/AddDishView.swift
+++ BestRecipes/Screens/AddDishView/AddDishView.swift
@@ -11,7 +11,8 @@ struct AddDishView: View {
     @State var title: String = ""
     @State var serves: Int = 0
     @State var cookTime: Int = 0
-    @State var ingredients: [Ingredient] = []
+    @State var ingredients: [UserIngredient] = [UserIngredient()]
+    @State var units = Unit.allCases.map {$0.rawValue}
     var body: some View {
         VStack {
             //ImagePicker
@@ -30,6 +31,69 @@ struct AddDishView: View {
             VStack {
                 AddDishElement(serves: true, value: $serves)
                 AddDishElement(serves: false, value: $cookTime)
+            }
+            VStack {
+                HStack {
+                    Text("Ingredients")
+                        .font(.custom(Font.semiBold, size: 20))
+                    Spacer()
+                }
+                ScrollView(.vertical, showsIndicators: false) {
+                    ForEach($ingredients, id: \.id) { ingredient in
+                        HStack {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.brGray)
+                                TextField("Item name", text: ingredient.title)
+                                    .font(.custom(Font.regular, size: 14))
+                                    .padding(.horizontal, 10)
+                            }
+                            .frame(width: 150)
+                            
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.brGray)
+                                TextField("amount", text: ingredient.amount)
+                                    .font(.custom(Font.regular, size: 14))
+                                    .padding(.horizontal, 10)
+                            }
+                            .frame(width: 70)
+                            Spacer()
+                            Button {
+                                switch ingredient.wrappedValue.unit {
+                                case "gr" : ingredient.wrappedValue.unit = "ml"
+                                case "ml": ingredient.wrappedValue.unit = "sp"
+                                case "sp": ingredient.wrappedValue.unit = "gr"
+                                default : ingredient.wrappedValue.unit = "gr"
+                                }
+                                
+                            } label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(lineWidth: 1)
+                                        .foregroundStyle(.brGray)
+                                    Text(ingredient.wrappedValue.unit)
+                                        .foregroundStyle(.black)
+                                        .font(.custom(Font.regular, size: 14))
+                                        .padding(.horizontal, 10)
+                                }
+                                
+                            }
+                            Button {
+                                
+                            } label: {
+                                ZStack {
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        .frame(height: 45)
+                    }
+                }
             }
             
             
